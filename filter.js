3899412b1c1ef5b3956fe9c5a92c9aa04312ac3b
David Schwarz <26146815+TheNyanta@users.noreply.github.com>
2019-05-21 15:12:45 +0200

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,1 +1,25 @@
+var invert = func() {
+var canvas = document.getElementById('canvas');
+context = canvas.getContext('2d');
 
+var imageData = context.getImageData(imageX, imageY, image.width, image.height);
+var dataArr = imageData.data;
+
+for(var i = 0; i < dataArr.length; i += 4)
+{
+    var r = dataArr[i]; // Red color lies between 0 and 255
+    var g = dataArr[i + 1]; // Green color lies between 0 and 255
+    var b = dataArr[i + 2]; // Blue color lies between 0 and 255
+    var a = dataArr[i + 3]; // Transparency lies between 0 and 255
+
+    var invertedRed = 255 - r;
+    var invertedGreen = 255 - g;
+    var invertedBlue = 255 - b;
+
+    dataArr[i] = invertedRed;
+    dataArr[i + 1] = invertedGreen;
+    dataArr[i + 2] = invertedBlue;
+}
+        
+context.putImageData(imageData, imageX, imageY);
+}
