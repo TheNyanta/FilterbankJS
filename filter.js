33923d3bab16dee92b26b68dc8c4c38472e3a6c4
David Schwarz <26146815+TheNyanta@users.noreply.github.com>
2019-05-21 14:54:02 +0200

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,1 +1,9 @@
-alert("Test")
+var name = prompt("Enter the name of the file", "backdrop.jpg");
+img.src = 'images/' + name;
+
+var img = new Image();
+img.onload = function() {
+    var ctx = document.getElementById('ctx').getContext('2d');
+    ctx.drawImage(img, 0, 0);
+}
+img.src = 'images/backdrop.jpg';
